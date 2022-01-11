9b00eeb5fecb97b0fef9eba8dcd0d49d76edbda2
Tanya Kalenik <tmolekyla@gmail.com>
2022-01-12 01:03:39 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let strReverse = String(Math.abs(n));
+    let result = "";
+    for (i = 0; i < strReverse.length; i++) {
+        result = `${strReverse[i]}${result}`;
+    }
+    return Number(result);
+};
